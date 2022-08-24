78ad70875b791378cead10d9bfb5e2202ef20de3
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-24 22:30:50 +0900

    style: lpのスタイル編集

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,12 @@
 module HomeHelper
   def features
     [
-      { title: 'Recruitment', body: '学校側が求める人物像、学力、資格、ポテンシャルが見れる！', icon: 'fa-solid fa-user-plus' },
-      { title: 'Research', body: '大学が行っている最先端の研究成果を見れる！', icon: 'fa-solid fa-book-open' },
-      { title: 'Chat', body: '教授とチャットをして、志望校選びの参考にしよう！', icon: 'fa-solid fa-comments' },
+      { title: 'Recruitment', body: 'あなたを求めている企業とマッチ', icon: 'fa-solid fa-user-plus' },
+      { title: 'Database', body: '1000種以上の資格情報を用意', icon: 'fa-solid fa-book-open' },
+      { title: 'Chat', body: '同志と励まし会おう', icon: 'fa-solid fa-comments' },
       { title: 'Free', body: '完全無料で利用することができます！', icon: 'fa-brands fa-creative-commons-nc-jp' },
-      { title: 'Security', body: '運営によるパトロールを実施しております。', icon: 'fa-solid fa-eye' },
-      { title: 'Next...', body: 'さぁ、未来を拓こう。', icon: 'fa-solid fa-door-open' }
+      { title: 'Security', body: '運営によるパトロール', icon: 'fa-solid fa-eye' },
+      { title: 'Next...', body: 'さぁ、未来を拓こう', icon: 'fa-solid fa-door-open' }
     ]
   end
 
