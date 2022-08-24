8052db917fc4219e34d737dd83945d348fb9cb69
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-24 23:31:35 +0900

    feat: genreコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'genres/show'
   root to: "home#index"
 
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :accounts
 
-  resources :categories
+  resources :categories, only: %i[index show]
+  resources :genres, only: %i[show]
 end
